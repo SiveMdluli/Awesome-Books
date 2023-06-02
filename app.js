ec743e11fcf35c8e94016c0f89c642fdba0e1e0c
Sive Mdluli <mdlulisive202@gmail.com>
2023-06-02 17:44:15 +0200

    Add error message and make sure project work as it should

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 class DOMElements {
   constructor() {
-    this.container = document.querySelector('.container');
-    this.header = document.querySelector('.Pagetitle');
-    this.form = document.querySelector('.form');
-    this.bookDisplay = document.querySelector('.bookDisplay');
-    this.inputTitle = document.querySelector('.title');
-    this.inputAuthor = document.querySelector('.author');
-    this.addButton = document.querySelector('.add');
-    this.error = document.createElement('span');
-    this.error.className = 'error';
-    this.error.textContent = 'Please fill out all fields.';
+    this.container = document.querySelector(".container");
+    this.header = document.querySelector(".Pagetitle");
+    this.form = document.querySelector(".form");
+    this.bookDisplay = document.querySelector(".bookDisplay");
+    this.inputTitle = document.querySelector(".title");
+    this.inputAuthor = document.querySelector(".author");
+    this.addButton = document.querySelector(".add");
+    this.error = document.createElement("span");
+    this.error.className = "error";
+    this.error.textContent = "Please fill out all fields.";
   }
 
   displayError() {
-    this.form.insertAdjacentElement('beforebegin', this.error);
+    const error = document.querySelector(".error");
+    error.innerHTML = "<p>Please fill in all fields</p>";
+  }
+
+  clearError() {
+    const error = document.querySelector(".error");
+    error.innerHTML = "";
   }
 }
 
 class LocalStorageHandler {
   constructor() {
-    this.bookHolderKey = 'storedBooks';
-    this.inputDataKey = 'inputdata';
+    this.bookHolderKey = "storedBooks";
+    this.inputDataKey = "inputdata";
   }
 
   getBookHolder() {
@@ -69,14 +75,14 @@ function addBookToHolder(newBook) {
 }
 
 function clearInputData() {
-  const inputData = { inputTitle: '', inputAuthor: '' };
+  const inputData = { inputTitle: "", inputAuthor: "" };
   localStorageHandler.saveInputData(inputData);
 }
 
 function addBook() {
   const titleData = domElements.inputTitle.value;
   const authorData = domElements.inputAuthor.value;
-  if (titleData === '' || authorData === '') {
+  if (titleData === "" || authorData === "") {
     domElements.displayError();
     return;
   }
@@ -84,20 +90,21 @@ function addBook() {
   addBookToHolder(newBook);
   clearInputData();
   displayBooks();
-  domElements.inputTitle.value = '';
-  domElements.inputAuthor.value = '';
+  domElements.inputTitle.value = "";
+  domElements.inputAuthor.value = "";
+  domElements.clearError(); // clear error message
 }
 
 function displayBooks() {
-  domElements.bookDisplay.innerHTML = '';
+  domElements.bookDisplay.innerHTML = "";
   const bookHolder = localStorageHandler.getBookHolder();
   bookHolder.forEach((book, index) => {
-    const bookInstance = document.createElement('article');
+    const bookInstance = document.createElement("article");
     const dispTitle = `"${book.title}" by ${book.author}`; // Concatenate book title and author
-    const delButton = document.createElement('button');
-    delButton.textContent = 'Remove';
-    delButton.setAttribute('data-index', index);
-    delButton.addEventListener('click', () => {
+    const delButton = document.createElement("button");
+    delButton.textContent = "Remove";
+    delButton.setAttribute("data-index", index);
+    delButton.addEventListener("click", () => {
       localStorageHandler.deleteBook(index);
       displayBooks();
     });
@@ -108,9 +115,9 @@ function displayBooks() {
 
     // add class to each bookInstance element based on its index
     if (index % 2 === 0) {
-      bookInstance.classList.add('book-row-even');
+      bookInstance.classList.add("book-row-even");
     } else {
-      bookInstance.classList.add('book-row-odd');
+      bookInstance.classList.add("book-row-odd");
     }
   });
 }
@@ -130,43 +137,63 @@ function setInputData() {
 }
 
 // Navigation function
-const sections = document.querySelectorAll('.content');
+const sections = document.querySelectorAll(".content");
 
 function navigateToSection(sectionId) {
-  const sections = document.querySelectorAll('.content');
+  const sections = document.querySelectorAll(".content");
   const section = document.querySelector(`#${sectionId}`);
   sections.forEach((s) => {
     if (s !== section) {
-      s.classList.remove('active');
-      s.style.display = 'none'; // hide non-active sections
+      s.classList.remove("active");
+      s.style.display = "none"; // hide non-active sections
     }
   });
-  section.classList.add('active');
-  section.style.display = 'block'; // show active section
+  section.classList.add("active");
+  section.style.display = "block"; // show active section
 
   // Handle special cases for Add and List sections
-  if (sectionId === 'add') {
-    domElements.header.textContent = 'Awesome Books';
-    domElements.form.style.display = 'block';
-    domElements.bookDisplay.style.display = 'none';
-  } else if (sectionId === 'list') {
-    domElements.header.textContent = 'Awesome Books';
-    domElements.form.style.display = 'none';
-    domElements.bookDisplay.style.display = 'block';
+  if (sectionId === "add") {
+    domElements.header.textContent = "Awesome Books";
+    domElements.form.style.display = "block";
+    domElements.bookDisplay.style.display = "none";
+  } else if (sectionId === "list") {
+    domElements.header.textContent = "Awesome Books";
+    domElements.form.style.display = "none";
+    domElements.bookDisplay.style.display = "block";
   } else {
     // Reset header and hide form and book display for other sections
-    domElements.header.textContent = ' Awesome Books';
-    domElements.form.style.display = 'none';
-    domElements.bookDisplay.style.display = 'none';
+    domElements.header.textContent = " Awesome Books";
+    domElements.form.style.display = "none";
+    domElements.bookDisplay.style.display = "none";
   }
 }
 
+// DATE AND TIME
+const timeElement = document.querySelector(".date");
+
+function updateTime() {
+  const date = new Date();
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: true,
+  };
+  const time = date.toLocaleString("en-US", options);
+  const timeElement = document.querySelector(".date");
+  timeElement.innerHTML = time;
+  setTimeout(updateTime, 1000);
+}
+
 // Event listners for the nav links
-const links = document.querySelectorAll('nav a');
+const links = document.querySelectorAll("nav a");
 links.forEach((link) => {
-  link.addEventListener('click', (event) => {
+  link.addEventListener("click", (event) => {
     event.preventDefault();
-    const sectionId = link.getAttribute('href');
+    const sectionId = link.getAttribute("href");
     navigateToSection(sectionId.substr(1));
   });
 });
@@ -174,14 +201,14 @@ links.forEach((link) => {
 function init() {
   updateTime();
   setInputData();
-  navigateToSection('add');
+  navigateToSection("add");
   displayBooks();
-  domElements.inputTitle.addEventListener('input', saveInputData);
-  domElements.inputAuthor.addEventListener('input', saveInputData);
-  domElements.addButton.addEventListener('click', addBook);
+  domElements.inputTitle.addEventListener("input", saveInputData);
+  domElements.inputAuthor.addEventListener("input", saveInputData);
+  domElements.addButton.addEventListener("click", addBook);
 
-  addBookToHolder(new Book('Book 1', 'Author 1'));
-  addBookToHolder(new Book('Book 2', 'Author 2'));
+  addBookToHolder(new Book("Book 1", "Author 1"));
+  addBookToHolder(new Book("Book 2", "Author 2"));
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
